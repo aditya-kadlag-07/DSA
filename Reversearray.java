ba98ca0cc730b8009295514c8ddfcb8221b139ff
Aditya Kadlag <adityakadlag2@gmail.com>
2025-08-26 12:31:15 +0530

    Reverse Array

---
+++ Reversearray.java
@@ -0,0 +1,25 @@
+//Given an array of integers, reverse the array without using extra space.
+
+public class Reversearray {
+    public static void main (String[] args){
+        int[] arr={1,2,3,4,5};
+
+
+        int start=0;
+        int end =arr.length-1;
+
+        while(start<end){
+
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+
+        }
+
+        for(int num:arr){
+            System.out.println(num + " ");
+        }
+    }
+}
