b23d2327b33b00898f7c4d6708aae648e9e6957d
aditya-kadlag-07 <adityakadlag2@gmail.com>
2025-09-25 11:26:44 +0530

    KadaneAlgo

---
+++ KadaneAlgo.java
@@ -0,0 +1,20 @@
+public class KadaneAlgo {
+    public static int maxSubArraySum(int[] nums) {
+        int maxSoFar = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Either take current element alone OR extend previous subarray
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSoFar = Math.max(maxSoFar, currentSum);
+        }
+
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray Sum = " + maxSubArraySum(arr));
+    }
+}
+
